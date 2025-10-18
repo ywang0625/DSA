f765abe161f29c93d5f2731ac44f1187f60b59c8
ywang0625 <130682075+ywang0625@users.noreply.github.com>
2025-10-18 11:21:42 -0700

    Time: 56 ms (48.96%) | Memory: 55.2 MB (16.02%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -4,9 +4,7 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    const p1 = await promise1;
-    const p2 = await promise2;
-    return p1 + p2;
+    return (await promise1) + (await promise2);
 };
 
 /**
