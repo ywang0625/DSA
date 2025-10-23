809711928a855d90fcef3981d97487d80ab6e059
ywang0625 <130682075+ywang0625@users.noreply.github.com>
2025-10-23 10:15:19 -0700

    Time: 50 ms (48.55%) | Memory: 55.8 MB (75.46%) - LeetSync

---
+++ 2798-chunk-array/chunk-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    var ans = []; 
+
+    for(let i = 0; i < arr.length; i+=size){
+        ans.push(arr.slice(i, i+size));
+    }
+
+    return ans;
+};
